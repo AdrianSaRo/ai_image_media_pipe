b1e7990c7542079fff1bdc3f25964dde52510a2f
Adrian Sanchez Romero <adriancruido@gmail.com>
2024-05-01 15:52:15 +0200

    gh pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://AdrianSaRo.github.io',
+  base: 'ai_image_media_pipe',
 });
